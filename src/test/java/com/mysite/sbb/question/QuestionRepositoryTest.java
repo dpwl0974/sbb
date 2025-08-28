646a14cf81e7004621a788d81d17d941c645c453
dpwl0974 <dpwl0974@gmail.com>
2025-08-28 11:18:14 +0900

    refactor: 테스트 도메인 분리

--- src/test/java/com/mysite/sbb/question/QuestionRepositoryTest.java
+++ src/test/java/com/mysite/sbb/question/QuestionRepositoryTest.java
@@ -2,8 +2,6 @@ package com.mysite.sbb.question;
 
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
-import com.mysite.sbb.question.Question;
-import com.mysite.sbb.question.QuestionRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
